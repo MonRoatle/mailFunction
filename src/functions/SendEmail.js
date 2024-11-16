499b40739060a644b86277893094bc6c2fe7fd83
tb13 <tb13@example.com>
2024-11-16 13:08:56 +0900

    send emailのスクリプト作成

--- src/functions/SendEmail.js
+++ src/functions/SendEmail.js
@@ -1,13 +1,47 @@
-const { app } = require('@azure/functions');
+const { EmailClient } = require("@azure/communication-email");
 
-app.http('SendEmail', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+const connectionString = "endpoint=https://fujitapjmailservice.japan.communication.azure.com/;accesskey=3Z14wruZDzzYJTD1VFEqL2vQnE8rJ0kijgb0c9h1IE6Mbscf1H5SJQQJ99AKACULyCpBPHK3AAAAAZCSiZAS";
+const client = new EmailClient(connectionString);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+module.exports = async function (context, req) {
+    const { emailAddress } = req.body;
 
-        return { body: `Hello, ${name}!` };
+    if (!emailAddress) {
+        context.res = {
+            status: 400,
+            body: "Email address is required",
+        };
+        return;
     }
-});
+
+    const emailMessage = {
+        senderAddress: "DoNotReply@4693a3f1-4f27-46f4-a5a0-21f8b80abbcd.azurecomm.net",
+        content: {
+            subject: "テスト メール",
+            plainText: "メールで Hello World。",
+            html: `
+            <html>
+                <body>
+                    <h1>メールで Hello World。</h1>
+                </body>
+            </html>`,
+        },
+        recipients: {
+            to: [{ address: emailAddress }],
+        },
+    };
+
+    try {
+        const poller = await client.beginSend(emailMessage);
+        await poller.pollUntilDone();
+        context.res = {
+            status: 200,
+            body: "Email sent successfully",
+        };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: `Failed to send email: ${error.message}`,
+        };
+    }
+};
